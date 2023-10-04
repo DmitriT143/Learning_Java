6956a1065f83eb512db82bd965c08fe296b74657
DmitriT143 <titkovdmt@gmail.com>
2023-10-04 17:11:26 +0300

    Lab 2 and 3 fixed and done

--- src/Conquer_two.java
+++ src/Conquer_two.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Conquer_two {
     public static void main(String[] args) {
@@ -15,15 +16,17 @@ public class Conquer_two {
         System.out.println(countRoots(new int[]{1,-3,2}));
         System.out.println(countRoots(new int[]{2,5,2}));
         System.out.println(countRoots(new int[]{1,-6,9}));
-        System.out.println(salesData(new String[]{"Apple", "Shop1", "Shop2", "Shop3", "Shop4"},
-                new String[]{"Banana", "Shop2", "Shop3", "Shop4"},
-                new String[]{"Orange", "Shop1", "Shop3", "Shop4"},
-                new String[]{"Pear", "Shop2", "Shop4"}));
-        System.out.println(salesData(new String[]{"Fridge", "Shop2", "Shop3"},
-                new String[]{"Microwave", "Shop1", "Shop2", "Shop3", "Shop4"},
-                new String[]{"Laptop", "Shop3", "Shop4",},
-                new String[]{"Phone", "Shop1", "Shop2", "Shop3", "Shop4"}));
-        System.out.println();
+        System.out.println(salesData(new String[][]{{"Apple", "Shop1", "Shop2", "Shop3", "Shop4"},{"Banana", "Shop2", "Shop3", "Shop4"},{"Orange", "Shop1", "Shop3", "Shop4"},{"Pear", "Shop2", "Shop4"}}));
+        System.out.println(salesData(new String[][]{{"Fridge", "Shop2", "Shop3"},{"Microwave", "Shop1", "Shop2", "Shop3", "Shop4"},{"Laptop", "Shop3", "Shop4"},{"Phone", "Shop1", "Shop2", "Shop3", "Shop4"}}));
+        System.out.println(validSplit("apple eagle egg goat"));
+        System.out.println(validSplit("cat dog goose fish"));
+        System.out.println(waveFormCheck(new int[]{3, 1, 4, 2, 7, 5}));
+        System.out.println(waveFormCheck(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(waveFormCheck(new int[]{1, 2, -6, 10, 3}));
+        System.out.println(commonVowel("Hello world"));
+        System.out.println(commonVowel("Actions speak louder than words."));
+        System.out.println(Arrays.deepToString(dataScience(new Integer[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 29, 10}, {5, 5, 5, 5, 35}, {7, 4, 3, 14, 2}, {1, 0, 11, 10, 1}})));
+        System.out.println(Arrays.deepToString(dataScience(new Integer[][]{{6, 4, 19, 0, 0}, {81, 25, 3, 1, 17}, {48, 12, 60, 32, 14}, {91, 47, 16, 65, 217}, {5, 73, 0, 4, 21}})));
     }
     public static String replaceVowels(String input){
         String[] vowels = new String[]{"a","e","y","i","o","u"};
@@ -43,33 +46,32 @@ public class Conquer_two {
         i++;}
         return String.join("", checkedText);
     }
-    public static String doesItFit(int a,int b,int c,int x,int y){
+    public static boolean doesItFit(int a,int b,int c,int x,int y){
         int[] toy = new int[]{a,b,c};
         int biggest = x>=y? x:y;
         int smallest = x<=y? x:y;
         for (int i = 0; i < 3;){
-            if (toy[i]<=smallest){toy[i] = biggest+1; for(int j = 0; j < 3;){if (toy[j]<=biggest){return "It does fit";}j++;}}
+            if (toy[i]<=smallest){toy[i] = biggest+1; for(int j = 0; j < 3;){if (toy[j]<=biggest){return true;}j++;}}
             i++;
         }
-        return "it doesn't";
+        return false;
     }
-    public static String numCheck(int input){
+    public static boolean numCheck(int input){
         int num = 0;
         int extra_num = input;
         while (extra_num > 0){
             num = (extra_num % 10)^2 + num ;
             extra_num = extra_num/10;
         }
-        if (input% 2 == num% 2){return "true";}
-        return "false";
+        if (input% 2 == num% 2){return true;}
+        return false;
     }
     public static int countRoots (int[] x){
         int a = x[0]; int b = x[1]; int c = x[2];
         int d = a^2 - 4 * b * c;
         if (d == 0){return d;} else if (d > 0) { return d;} else {return 0;}
     }
-    public static String salesData(String[] array0,String[] array1,String[] array2,String[] array3){
-        String[][] input = {array0 ,array1, array2, array3};
+    public static String salesData(String[][] input){
         int shops = 0; int not_loose = 0;
         ArrayList<String> shopItems = new ArrayList<>();
         for (int i = 0; i < input.length;) {
@@ -82,5 +84,56 @@ public class Conquer_two {
             return shopItems.toString();
         }
         return "no multiple sales found";
+    }
+    public static boolean validSplit(String input){
+        String[] wordByWord = input.split(" ");
+        for (int i = 1; i < wordByWord.length; i++) {
+            if (!wordByWord[i].split("")[0].equals(wordByWord[i-1].split("")[wordByWord[i-1].length()-1])){return false;}
+        }
+        return true;
+    }
+    public static boolean waveFormCheck (int[] input){
+        boolean Up;
+        if (input[0] > input[1]){Up = true;}
+        else {Up = false;}
+        for (int i = 1; i < input.length-1; i++) {
+            if (Up && input[i] < input[i+1]){Up = false;}
+            else if (!Up && input[i] > input[i+1]){Up = true;}
+            else return false;
+        }
+        return true;
+    }
+    public static String commonVowel(String sentence) {
+        String MaxOccurring = "";
+        int MaxOccurred = 0;
+        String Vowels = "aeioyu";
+        String[] assorted = sentence.toLowerCase().replace(" ","").split("");
+        for (int i = 0; i < assorted.length; i++) {
+            if (Vowels.contains(assorted[i])) {
+                String currentlySorting = assorted[i];
+                int Occurrences = 1;
+                for (int j = 0; j < assorted.length; j++) {
+                    if (currentlySorting.equals(assorted[j])) {
+                        Occurrences++;
+                    }
+                }
+                if (Occurrences > MaxOccurred) {
+                    MaxOccurred = Occurrences;
+                    MaxOccurring = currentlySorting;
+                };
+            }
+        }
+        return MaxOccurring;
+    }
+    public static Integer[][] dataScience(Integer[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            int changeTo = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                if(j!=i){changeTo = changeTo+matrix[j][i];}
+            }
+            matrix[i][i] = changeTo/4;
+        }
+
+        return matrix;
     }
 }
