2ab286697879fc3ff462c8f05a28bfb8ab1790ba
DmitriT143 <titkovdmt@gmail.com>
2023-11-07 10:23:32 +0300

    Task 4 done (maybe)

--- src/TryLab4Catch/Test.java
+++ src/TryLab4Catch/Test.java
@@ -1,10 +1,13 @@
 package TryLab4Catch;
+import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) {
         AgeCheck user = new AgeCheck();
         try {
             user.setAge(-5);
-        } catch (CustomAgeException e){System.out.println(e);}
+        } catch (CustomAgeException e){System.out.println(e);} catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
