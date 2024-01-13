fad30285ce88189d809aa5023530eabd97068fed
DmitriT143 <titkovdmt@gmail.com>
2024-01-13 13:41:14 +0300

    Renamed files and Labs

---
+++ src/Task3.java
@@ -0,0 +1,165 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Answers to third practise work");
+        System.out.println("Task 1");
+        System.out.println(replaceVowels("Even if you did the task not by yourself, you have to understand every line of code"));
+        System.out.println(replaceVowels("apple"));
+        System.out.println("Task 2");
+        System.out.println(doublingSeason("hello"));
+        System.out.println(doublingSeason("beekeeper and bookkeeper with iing season"));
+        System.out.println("Task 3");
+        System.out.println(doesItFit(1,3,5,4,5));
+        System.out.println(doesItFit(1,8,1,1,1));
+        System.out.println(doesItFit(1,2,2,1,1));
+        System.out.println("Task 4");
+        System.out.println(numCheck(243));
+        System.out.println(numCheck(52));
+        System.out.println("Task 5");
+        System.out.println(countRoots(new int[]{1,-3,2}));
+        System.out.println(countRoots(new int[]{2,5,2}));
+        System.out.println(countRoots(new int[]{1,-6,9}));
+        System.out.println("Task 6");
+        System.out.println(salesData(new String[][]{{"Apple", "Shop1", "Shop2", "Shop3", "Shop4"},{"Banana", "Shop2", "Shop3", "Shop4"},{"Orange", "Shop1", "Shop3", "Shop4"},{"Pear", "Shop2", "Shop4"}}));
+        System.out.println(salesData(new String[][]{{"Fridge", "Shop2", "Shop3"},{"Microwave", "Shop1", "Shop2", "Shop3", "Shop4"},{"Laptop", "Shop3", "Shop4"},{"Phone", "Shop1", "Shop2", "Shop3", "Shop4"}}));
+        System.out.println("Task 7");
+        System.out.println(validSplit("apple eagle egg goat"));
+        System.out.println(validSplit("cat dog goose fish"));
+        System.out.println("Task 8");
+        System.out.println(waveFormCheck(new int[]{3, 1, 4, 2, 7, 5}));
+        System.out.println(waveFormCheck(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(waveFormCheck(new int[]{1, 2, -6, 10, 3}));
+        System.out.println("Task 9");
+        System.out.println(commonVowel("Hello world"));
+        System.out.println(commonVowel("Actions speak louder than words."));
+        System.out.println("Task 10");
+        System.out.println(Arrays.deepToString(dataScience(new Integer[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 29, 10}, {5, 5, 5, 5, 35}, {7, 4, 3, 14, 2}, {1, 0, 11, 10, 1}})));
+        System.out.println(Arrays.deepToString(dataScience(new Integer[][]{{6, 4, 19, 0, 0}, {81, 25, 3, 1, 17}, {48, 12, 60, 32, 14}, {91, 47, 16, 65, 217}, {5, 73, 0, 4, 21}})));
+    }
+    public static String replaceVowels(String input){
+        String[] vowels = new String[]{"a","e","y","i","o","u"};
+        for (int i = 0; i < vowels.length;) {
+            input = input.toLowerCase().replace(vowels[i], "*");
+            i++;
+        }
+        return input;
+    }
+    public static String doublingSeason (String text){
+        String[] checkedText = text.split("");
+        for (int i = 0; i < checkedText.length-1;) {
+            if(checkedText[i].equals(checkedText[i + 1])) {
+                    checkedText[i] = "Double";
+                    checkedText[i + 1] = checkedText[i + 1].toUpperCase();
+                }
+        i++;}
+        return String.join("", checkedText);
+    }
+    public static boolean DoesItFit_mistook(int a,int b,int c,int x,int y){
+        int[] toy = new int[]{a,b,c};
+        int biggest = x>=y? x:y;
+        int smallest = x<=y? x:y;
+        for (int i = 0; i < 3;){
+            if (toy[i]<=smallest){toy[i] = biggest+1; for(int j = 0; j < 3;){if (toy[j]<=biggest){return true;}j++;}}
+            i++;
+        }
+        return false;
+    }
+    // без циклов, но с работой с контейнером
+    public static boolean doesItFit(int a, int b,int c,int x,int y){
+        int[] toy = new int[]{a,b,c};
+        int[] hole = new int[]{x,y};
+        Arrays.sort(toy);
+        Arrays.sort(hole);
+        return (toy[0] <= hole[0] && toy[1] <= hole[1]);
+    }
+    public static boolean numCheck(int input){
+        int num = 0;
+        int extra_num = input;
+        while (extra_num > 0){
+            num = (extra_num % 10)^2 + num ;
+            extra_num = extra_num/10;
+        }
+        return  (input% 2 == num% 2);
+    }
+    public static int countRoots (int[] x){
+        int a = x[0]; int b = x[1]; int c = x[2];
+        int d = b*b - (4 * a * c);
+        if (d == 0){if (b/a % 2 == 0){return 1;}
+            return 0;
+        }
+        else if (d > 0) {
+            double L = (Math.sqrt(d) - b)/(2*a);
+            double R = (-Math.sqrt(d) - b)/(2*a);
+            if(R%1 == 0&& L%1 == 0){ return 2;}
+            else if (R%1 ==0 || L%1 ==0){return 1;}
+            else {return 0;}
+        }
+        else {return 0;}
+    }
+    public static String salesData(String[][] input){
+        int shops = 0; int not_loose = 0;
+        ArrayList<String> shopItems = new ArrayList<>();
+        for (int i = 0; i < input.length;) {
+            if (input.length > shops){shops = input[i].length;}
+            i++;}
+        for (int i = 0; i < input.length;) {
+            if (input[i].length == shops){shopItems.add(input[i][0]); not_loose = 1;}
+        i++;}
+        if (not_loose == 1) {
+            return shopItems.toString();
+        }
+        return "no multiple sales found";
+    }
+    public static boolean validSplit(String input){
+        String[] wordByWord = input.split(" ");
+        for (int i = 1; i < wordByWord.length; i++) {
+            if (!wordByWord[i].split("")[0].equals(wordByWord[i-1].split("")[wordByWord[i-1].length()-1])){return false;}
+        }
+        return true;
+    }
+    public static boolean waveFormCheck (int[] input){
+        boolean Up;
+        if (input[0] > input[1]){Up = true;}
+        else {Up = false;}
+        for (int i = 1; i < input.length-1; i++) {
+            if (Up && input[i] < input[i+1]){Up = false;}
+            else if (!Up && input[i] > input[i+1]){Up = true;}
+            else return false;
+        }
+        return true;
+    }
+    public static String commonVowel(String sentence) {
+        String MaxOccurring = "";
+        int MaxOccurred = 0;
+        String Vowels = "aeioyu";
+        String[] assorted = sentence.toLowerCase().replace(" ","").split("");
+        for (int i = 0; i < assorted.length; i++) {
+            if (Vowels.contains(assorted[i])) {
+                String currentlySorting = assorted[i];
+                int Occurrences = 1;
+                for (int j = 0; j < assorted.length; j++) {
+                    if (currentlySorting.equals(assorted[j])) {
+                        Occurrences++;
+                    }
+                }
+                if (Occurrences > MaxOccurred) {
+                    MaxOccurred = Occurrences;
+                    MaxOccurring = currentlySorting;
+                };
+            }
+        }
+        return MaxOccurring;
+    }
+    public static Integer[][] dataScience(Integer[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            int changeTo = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                if(j!=i){changeTo = changeTo+matrix[j][i];}
+            }
+            matrix[i][i] = changeTo/4;
+        }
+        return matrix;
+    }
+}
---
+++ src/Task5.java
@@ -0,0 +1,275 @@
+import java.util.*;
+import java.time.*;
+
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("Answers to fifth practise work");
+        System.out.println("1)" + SamePattern("ABAB", "CDCD"));
+        System.out.println("1)" + SamePattern("ABCBA", "BCDCB"));
+        System.out.println("1)" + SamePattern("EDCBA", "IHGFE"));
+        System.out.println("1)" + SamePattern("FFGG", "CDCD"));
+        System.out.println("1)" + SamePattern("FFFF", "ABCD"));
+        System.out.println();
+        System.out.println("2)" + SpiderPath("H3", "E2"));
+        System.out.println("2)" + SpiderPath("A4", "B2"));
+        System.out.println("2)" + SpiderPath("B4", "H2"));
+        System.out.println("2)" + SpiderPath("C4", "H2"));
+        System.out.println("2)" + SpiderPath("A2", "C4"));
+        System.out.println();
+        System.out.println("3)" + recursiveNumLength("4666"));
+        System.out.println("3)" + recursiveNumLength("544"));
+        System.out.println("3)" + recursiveNumLength("121317"));
+        System.out.println("3)" + recursiveNumLength("0"));
+        System.out.println("3)" + recursiveNumLength("12345"));
+        System.out.println("3)" + recursiveNumLength("1289396387328L"));
+        System.out.println();
+        System.out.println("4)" + PiratedScrambleCounter(new String[]{"cat", "create", "sat"},"caster"));
+        System.out.println("4)" + PiratedScrambleCounter(new String[]{"trance", "recant"}, "recant"));
+        System.out.println("4)" + PiratedScrambleCounter(new String[]{"dote", "dotes", "toes", "set", "dot", "dots", "sted"}, "tossed"));
+        System.out.println();
+        System.out.println("5)" + Arrays.deepToString(upTo8(new Integer[]{1, 2, 3, 4, 5})));
+        System.out.println("5)" + Arrays.deepToString(upTo8(new Integer[]{1, 2, 3, 7, 9})));
+        System.out.println("5)" + Arrays.deepToString(upTo8(new Integer[]{10, 9, 7, 2, 8})));
+        System.out.println("5)" + Arrays.deepToString(upTo8(new Integer[]{1, 6, 5, 4, 8, 2, 3, 7})));
+        System.out.println();
+        System.out.println("6)" + takeDownBy5(new int[]{95, 83, 90, 87, 88, 93}) );
+        System.out.println("6)" + takeDownBy5(new int[]{10}));
+        System.out.println("6)" + takeDownBy5(new int[]{53,79}));
+        System.out.println();
+        System.out.println("7)" + Ceasar("hello world",true,3));
+        System.out.println("7)" + Ceasar("KHOOR ZRUOG",false,3));
+        System.out.println("7)" + Ceasar("almost last task!",true,4));
+        System.out.println("7)" + Ceasar("EPQSWX PEWX XEWO",false,4));
+        System.out.println();
+        System.out.println("8)" + placementWithoutRepeat(5, 3));
+        System.out.println("8)" + placementWithoutRepeat(7, 3));
+        System.out.println();
+        System.out.println("9)" + dateCityToCity("Los Angeles","April 1 2011, 23:23","Canberra") );
+        System.out.println("9)" + dateCityToCity("London","July 31, 1983 23:01","Rome") );
+        System.out.println("9)" + dateCityToCity("New York","December 31, 1970, 13:40","Beijing") );
+        System.out.println();
+        System.out.println("10)" + isSmallest(3));
+        System.out.println("10)" + isSmallest(30));
+        System.out.println("10)" + isSmallest(321));
+        System.out.println("10)" + isSmallest(123));
+        System.out.println("10)" + isSmallest(1307));
+    }
+
+    // Pattern goes like <>< or >><< or =>= or any other
+    // May use CharNumber to get proper pattern like > < =
+    public static boolean SamePattern(String Pattern, String CheckOn){
+        String[] PatternSplit = Pattern.split("");
+        String[] PatternCopySplit = CheckOn.split("");
+        for (int i = 0; i < Pattern.length()-1; i++){
+            if ((int)CheckOn.charAt(i) - (int)CheckOn.charAt(i+1) != (int)Pattern.charAt(i)-(int)Pattern.charAt(i+1))
+            {return false;};
+        }
+        return true;
+    }
+
+    //Spider has a Net it goes like A-00 B-45 C-90 D-135 E-180 F-225 G-270 H-315
+    //Poses by R goes by 1-4 so eh. we have sin and R that's it
+    //Length L by ABCDEFGH = 2*r*0,707 or goTo R for 1 per R
+    //Exceptions handling are as follows: Is A/B->H/G; lower radian to upper radian or just go by radius if degree < 91
+    //Lastly if Degree > 91 go by center
+    //degrees would be counted as stated above
+    public static String SpiderPath(String spiderPos, String flyPos){
+        String[] curFlyPos = flyPos.split("");
+        String[] curSpiderPos = spiderPos.split("");
+        String path = spiderPos;
+        String exceptionSpider= "AB";
+        String exceptionFly = "GH";
+        int diffBetweenRad = Math.abs((int) curFlyPos[0].charAt(0) - (int) curSpiderPos[0].charAt(0));
+        int diffBetweenRadius = (int) curFlyPos[1].charAt(0) - (int) curSpiderPos[1].charAt(0);
+        if(diffBetweenRad > 5 && exceptionSpider.contains(curSpiderPos[0]) && exceptionFly.contains(curFlyPos[0]))
+        {
+            while (diffBetweenRadius < 0){
+                curSpiderPos[1] = String.valueOf(Integer.parseInt(curSpiderPos[1]) - 1);
+                diffBetweenRadius++;
+                path = path + ","+String.join("",curSpiderPos);
+            }
+            while (!curSpiderPos[0].equals("A")){
+                curSpiderPos[0] = Character.toString((int) curSpiderPos[0].charAt(0)-1);
+                path = path + ","+String.join("",curSpiderPos);
+            }
+            curSpiderPos[0] = "H";
+            diffBetweenRad = Math.abs((int) curFlyPos[0].charAt(0) - (int) curSpiderPos[0].charAt(0));
+            path = path + ","+String.join("",curSpiderPos);
+        }
+        if(diffBetweenRad <= 2){
+            while (diffBetweenRadius < 0){
+                curSpiderPos[1] = String.valueOf(Integer.parseInt(curSpiderPos[1]) - 1);
+                diffBetweenRadius++;
+                path = path + ","+String.join("",curSpiderPos);
+            }
+            while (diffBetweenRad != 0){
+                curSpiderPos[0] = ((int) curFlyPos[0].charAt(0) - (int) curSpiderPos[0].charAt(0)>0)? Character.toString(curSpiderPos[0].charAt(0)+1):Character.toString(curSpiderPos[0].charAt(0)-1);
+                diffBetweenRad = Math.abs((int) curFlyPos[0].charAt(0) - (int) curSpiderPos[0].charAt(0));
+                path = path + ","+String.join("",curSpiderPos);
+            }
+            while (diffBetweenRadius > 0){
+                curSpiderPos[1] = String.valueOf(Integer.parseInt(curSpiderPos[1]) + 1);
+                diffBetweenRadius--;
+                path = path + ","+String.join("",curSpiderPos);
+            }        }
+        else{
+            for (int i = 0; i < Integer.parseInt(spiderPos.substring(1)); i++) {
+                curSpiderPos[1] = String.valueOf(Integer.parseInt(curSpiderPos[1])-1);
+                path = path + ","+String.join("",curSpiderPos);
+            }
+            curSpiderPos[0] = curFlyPos[0];
+            for (int i = 0; i < Integer.parseInt(flyPos.substring(1)); i++) {
+                curSpiderPos[1] = String.valueOf(Integer.parseInt(curSpiderPos[1])+1);
+                path = path + "," + String.join("",curSpiderPos);
+            }
+        }
+        return path;
+    }
+    public static Integer recursiveNumLength(String b){
+        try {
+            long a = Long.parseLong(b);
+            if (a > 10)
+                return 1 + recursiveNumLength(String.valueOf(a/10));
+            else return 1;}
+        catch (NumberFormatException e){ return 0 + recursiveNumLength(b.substring(0, b.length()-1));}
+    }
+
+
+    //first find anagrams (+50)
+    //Second take away not correct words
+    //Than translate each word to points like word.len - 3 + bonuses beforehand
+    public static int PiratedScrambleCounter(String[] guessed, String word){
+        int score = 0;
+        boolean isGuessed = false;
+        for (int i = 0; i < guessed.length; i++) {
+            if(word.contains(guessed[i])){ isGuessed = true;}
+        }
+        String[] forAnagram = word.split("");
+        Arrays.sort(forAnagram);
+        for (int i = 0; i < guessed.length; i++) {
+            String[] wordle = guessed[i].split(""); Arrays.sort(wordle);
+            if (Arrays.equals(wordle, forAnagram) && isGuessed){score=score+50+guessed[i].length()-2;}
+            else{ boolean right = true;
+                String check = word;
+                for (int j = 0; j < guessed[i].length(); j++){
+                    if (!check.contains(wordle[j])){right = false;}
+                    else {int remove = check.indexOf(wordle[j]); check = check.substring(0,remove) + check.substring(remove+1);}
+                }
+                if (right){score = score + guessed[i].length()-2;}
+            }
+        }
+        return score;
+    }
+    public static Integer[][] upTo8(Integer[] listed){Integer[] temp = new Integer[0];int TopLim = 1;int DownLim = 0;
+        ArrayList<Integer[]> toOut = new ArrayList<>();
+        while (TopLim < listed.length){
+            if(listed[TopLim]+listed[DownLim] == 8) {
+                temp= new Integer[]{listed[TopLim], listed[DownLim]};Arrays.sort(temp); toOut.add(temp);
+            }
+            DownLim++;
+            if(DownLim == TopLim){
+                TopLim++;DownLim = 0;
+            }
+        }
+        Integer[][] out = new Integer[toOut.size()][];
+        for (int i = 0; i < toOut.size(); i++){
+            out[i] = toOut.get(i);
+        }
+        return out;
+    }
+    public static String takeDownBy5(int[] grades){
+        int myGrade;
+        int Prev = 0;
+        for(int i = 0; i < grades.length; i++)
+            Prev = Prev + grades[i];
+        myGrade = Prev / (grades.length) - 5 * (grades.length+1);
+        return "\""+myGrade +"%\"";
+    }
+    public static String Ceasar(String text, boolean encode, int movedBy){
+        if (Math.abs(movedBy)>26){movedBy = movedBy%26;}
+        if(!encode){movedBy = -movedBy;}
+        text = text.toUpperCase();
+        String MustBeMoved = text.replaceAll("[^A-Z]","");
+        String[] Placeholder =MustBeMoved.split("");
+        if (movedBy > 0){Arrays.sort(Placeholder, Collections.reverseOrder());}
+        else Arrays.sort(Placeholder);
+        for (int i = 0; i < Placeholder.length-1; i++) {
+            if (Placeholder[i].equals(Placeholder[i+1])){Placeholder[i] = "";}
+        }
+        MustBeMoved = String.join("",Placeholder);
+        System.out.println(text);
+        for (int i = 0; i < MustBeMoved.length(); i++) {char newChar; char oldChar = MustBeMoved.charAt(i);
+            if (oldChar + movedBy > 90){newChar = (char)(oldChar+movedBy+6);}
+            else if(oldChar + movedBy < 65){newChar = (char)(oldChar+movedBy+58);}
+            else {newChar = (char)(oldChar + movedBy);}
+            text = text.replace(oldChar,newChar);
+        }
+        text = text.toUpperCase();
+        return text;
+    }
+    public static int placementWithoutRepeat(int n, int k){
+        int factorialn = 1; int factorialk = 1;
+        for(int fact = 2; fact <= n; fact++) {
+            factorialn *= fact;
+            if (fact == (n - k)){ factorialk = factorialn;}
+        }
+    k = factorialn/factorialk;
+    return k;
+    }
+    public static String dateCityToCity (String cityA, String date, String cityB){
+        String[] Month = new String[]{"January","February","March","April","May","June","July","August","September","October","November","December"};
+        Hashtable<String, Double> timeCity = new Hashtable<>();
+        timeCity.put("Los Angeles",-8.00);
+        timeCity.put("New York",-5.00);
+        timeCity.put("Caracas",-4.50);
+        timeCity.put("Buenos Aires",-3.00);
+        timeCity.put("London",-0.00);
+        timeCity.put("Rome",+1.00);
+        timeCity.put("Moscow",+3.00);
+        timeCity.put("Tehran",+3.50);
+        timeCity.put("New Delhi",+5.50);
+        timeCity.put("Beijing",+8.00);
+        timeCity.put("Canberra",+10.00);
+        String[] DatInfo = date.replace(",","").split(" "); // 0 - M 1 - DD 2 - YYYY 3-Time (H - M)
+        for (int i = 0; i < Month.length; i++) {
+            if (DatInfo[0].equals(Month[i])){
+                DatInfo[0] = String.valueOf(i+1);}
+        }
+        Double time = Double.parseDouble(DatInfo[3].replace(":",".")) - timeCity.get(cityA) + timeCity.get(cityB);
+        if (time > 24.00){
+            time = time - 24.00; DatInfo[1]=String.valueOf(Integer.parseInt(DatInfo[1])+1);}
+        else if (time < 00.00){
+            time = time + 24.00; DatInfo[1]=String.valueOf(Integer.parseInt(DatInfo[1])-1);}
+        Month referenceMonth = java.time.Month.of(Integer.parseInt(DatInfo[0]));
+        YearMonth yearMonth = YearMonth.of(Integer.parseInt(DatInfo[2]),referenceMonth); int minMonthLenght = yearMonth.lengthOfMonth();
+        if (Integer.parseInt(DatInfo[1]) > minMonthLenght) {
+            DatInfo[1] = String.valueOf(Integer.parseInt(DatInfo[1]) - minMonthLenght); DatInfo[0] = String.valueOf(Integer.parseInt(DatInfo[0])+1);}
+        if (Integer.parseInt(DatInfo[1]) <= 0)
+        {
+            DatInfo[1] = String.valueOf(Integer.parseInt(DatInfo[1])-1);
+            referenceMonth = java.time.Month.of(Integer.parseInt(DatInfo[1]));
+            yearMonth = YearMonth.of(Integer.parseInt(DatInfo[2]),referenceMonth);
+            minMonthLenght = yearMonth.lengthOfMonth();
+            DatInfo[1] = String.valueOf(minMonthLenght);
+        }
+        if(Integer.parseInt(DatInfo[0])>12){
+            DatInfo[0]="1"; DatInfo[2] = String.valueOf(Integer.parseInt(DatInfo[2])+1);}
+        if(Integer.parseInt(DatInfo[0])<0){
+            DatInfo[0]="11"; DatInfo[2] = String.valueOf(Integer.parseInt(DatInfo[2])-1);}
+        DatInfo[0] = String.valueOf(Integer.parseInt(DatInfo[0]));
+        return DatInfo[2]+"-"+DatInfo[0]+"-"+DatInfo[1]+" "+ String.format("%.2f", time).replace(",",":");
+    }
+    public static boolean isSmallest(int input){
+        String[] Placeholder = String.valueOf(input).split(""); int ZeroCounter = 0;
+        Integer[] unChanged = new Integer[Placeholder.length]; ArrayList<Integer> Egor = new ArrayList<>();
+        for (int i = 0; i < Placeholder.length; i++){
+            unChanged[i] = Integer.parseInt(Placeholder[i]);
+            if (Integer.parseInt(Placeholder[i])==0){ZeroCounter++;} else Egor.add(Integer.parseInt(Placeholder[i]));
+        }Collections.sort(Egor);
+        while (ZeroCounter!=0){ZeroCounter--;Egor.add(1,0);}
+        Integer[] glued = Egor.toArray(new Integer[0]);
+        if (Arrays.equals(glued, unChanged))
+            return true;
+        return false;
+    }
+}
